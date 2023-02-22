2aaea7233bb4d0f3a66eef0b5bb583a36f80c5cf
TheKohan <kohan142011@gmail.com>
2023-02-22 19:37:55 +0100

    feat: markdown plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,48 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
+import remarkCodeTitles from "remark-code-titles";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
 export default defineConfig({
+  site: `https://kohan.dev/`,
+  markdown: {
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noreferrer noopener"],
+          content: {
+            type: "text",
+            value: "↗",
+          },
+        },
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            class: "heading-link heading-link--hidden---effects",
+            "data-heading-link": true,
+          },
+          behavior: "wrap",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "rose-pine",
+      wrap: true,
+    },
+  },
   integrations: [
     tailwind({
       config: {
@@ -14,5 +51,6 @@ export default defineConfig({
     }),
     react(),
     mdx(),
+    sitemap(),
   ],
 });
