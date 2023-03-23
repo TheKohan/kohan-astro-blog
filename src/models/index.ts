ac84a6ac0aa26b4882d750e49525a7292593187a
TheKohan <kohan142011@gmail.com>
2023-03-23 20:41:47 +0100

    feat: pagination props

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,3 +7,30 @@ export type Frontmatter = {
   tags?: string[];
   author?: string;
 };
+export type PaginatorProps = {
+  /** url of the current page */
+  current: string;
+  /** url of the previous page (if there is one) */
+  prev: string | undefined;
+  /** url of the next page (if there is one) */
+  next: string | undefined;
+};
+export type Page<T = any> = {
+  /** result */
+  data: T[];
+  /** metadata */
+  /** the count of the first item on the page, starting from 0 */
+  start: number;
+  /** the count of the last item on the page, starting from 0 */
+  end: number;
+  /** total number of results */
+  total: number;
+  /** the current page number, starting from 1 */
+  currentPage: number;
+  /** number of items per page (default: 25) */
+  size: number;
+  /** number of last page */
+  lastPage: number;
+
+  url: PaginatorProps;
+};
