8e46753d10eef59e7212b39dd5b2b0925fd09dbb
TheKohan <kohan142011@gmail.com>
2023-04-17 21:01:14 +0200

    feat: markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,43 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
-import remarkCodeTitles from "remark-code-titles";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import image from "@astrojs/image";
 import { remarkReadingTime } from "./src/plugins/remark-read-time-plugin.mjs";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const prettyCodeOptions = {
+  theme: "one-dark-pro",
+  keepBackground: true,
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word"];
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: `https://kohan.dev/`,
   markdown: {
-    remarkPlugins: [remarkCodeTitles, remarkReadingTime],
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
+      [rehypePrettyCode, prettyCodeOptions],
       [
         rehypeExternalLinks,
         {
@@ -41,14 +66,16 @@ export default defineConfig({
         },
       ],
     ],
-    shikiConfig: {
-      theme: "one-dark-pro",
-      wrap: true,
-    },
+    // shikiConfig: {
+    //   theme: "one-dark-pro",
+    //   wrap: true,
+    // },
   },
   integrations: [
     react(),
-    mdx({ remarkPlugins: [remarkCodeTitles, remarkReadingTime] }),
+    mdx({
+      remarkPlugins: [remarkReadingTime],
+    }),
     sitemap(),
     tailwind(),
     image(),
